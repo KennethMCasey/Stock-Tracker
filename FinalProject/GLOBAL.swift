593cbab612a8299aec017b5c89ace3b3a7c77d68
Kenny Casey <kennycasey@kennys-mac-pro.local>
2019-05-04 20:29:21 -0400

    base

---
+++ FinalProject/GLOBAL.swift
@@ -0,0 +1,13 @@
+//
+//  GLOBAL.swift
+//  FinalProject
+//
+//  Created by Kenny Casey on 5/4/19.
+//  Copyright © 2019 Kenny Casey. All rights reserved.
+//
+
+import Foundation
+
+struct GLOBAL {
+    let APIKey = "F5F2RN5H1P2FICV6"
+}
