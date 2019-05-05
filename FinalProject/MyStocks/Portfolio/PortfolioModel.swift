edc58adb559cb149ea060c402fd742b8acb786c4
Kenny Casey <kennycasey@kennys-mac-pro.local>
2019-05-05 00:12:17 -0400

    Story Board Layout: Original

---
+++ FinalProject/MyStocks/Portfolio/PortfolioModel.swift
@@ -0,0 +1,9 @@
+//
+//  PortfolioModel.swift
+//  FinalProject
+//
+//  Created by Kenny Casey on 5/4/19.
+//  Copyright © 2019 Kenny Casey. All rights reserved.
+//
+
+import Foundation
---
+++ FinalProject/Settings/AddStock/AddStockModel.swift
@@ -0,0 +1,9 @@
+//
+//  AddStockModel.swift
+//  FinalProject
+//
+//  Created by Kenny Casey on 5/5/19.
+//  Copyright © 2019 Kenny Casey. All rights reserved.
+//
+
+import Foundation
